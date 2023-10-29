22cb276f8724a06725d0d8ea86d4e314d13cbfe7
Ivelina Plocheva <ivelina.plocheva@newredo.com>
2023-10-29 14:24:43 +0000

    Add api folder.

---
+++ api/getWord.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+
+// Creating a new endpoint to fetch the word from the external API
+app.get('/getWord', async (req, res) => {
+    console.log("In get word");
+    console.log(process.env.API_KEY);
+  try {
+    const response = await axios.get('https://api.api-ninjas.com/v1/randomword?type=noun', {
+      headers: {
+        'X-Api-Key': process.env.API_KEY,
+      },
+    });
+
+    res.json(response.data); // Sending the data as a JSON response
+  } catch (error) {
+    res.status(500).send(error.message || error);
+  }
+});
+
+module.exports = app;
