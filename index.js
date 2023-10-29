ef365d807b13de5d91ba1973e8505b792fbf908e
Ivelina Plocheva <ivelina.plocheva@newredo.com>
2023-10-29 14:08:37 +0000

    Export the express app.

--- index.js
+++ index.js
@@ -27,3 +27,6 @@ app.get('/getWord', async (req, res) => {
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
+
+// Export the Express API
+module.exports = app;
